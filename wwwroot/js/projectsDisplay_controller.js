08c5b39fb504c8b9e7e1f7dcb52a3ccaa59c0a34
avondas <aviendahooft@hotmail.com>
2024-01-21 15:09:02 +0100

    added styling to the projects page

---
+++ wwwroot/js/projectsDisplay_controller.js
@@ -0,0 +1,19 @@
+﻿const toggleBlisDigital = document.querySelector('.toggle_blis');
+const blisDigitalText = document.querySelector('.blis_digital');
+    
+const toggleDice = document.querySelector('.toggle_dice');
+const diceText = document.querySelector('.dice_roller');
+
+toggleBlisDigital.onclick = function() {
+    if (diceText.classList.contains('open')) {
+        diceText.classList.toggle('open');
+    }
+    blisDigitalText.classList.toggle('open');
+}
+
+toggleDice.onclick = function() {
+    if (blisDigitalText.classList.contains('open')) {
+        blisDigitalText.classList.toggle('open');
+    }
+    diceText.classList.toggle('open');
+}
