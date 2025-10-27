b52c1941a8e82843baa6e295c59f78d24c8e02ef
Avienda 't Hooft <aviendahooft@hotmail.com>
2025-10-27 21:28:12 +0100

    updated Projects and styling

--- js/projectsDisplay_controller.js
+++ js/projectsDisplay_controller.js
@@ -1,19 +1,38 @@
 ﻿const toggleBlisDigital = document.querySelector('.toggle_blis');
 const blisDigitalText = document.querySelector('.blis_digital');
     
-const toggleDice = document.querySelector('.toggle_dice');
-const diceText = document.querySelector('.dice_roller');
+const toggleBook = document.querySelector('.toggle_book');
+const bookText = document.querySelector('.book_app');
+
+const toggleDaggerHeart = document.querySelector('.toggle_dh');
+const daggerheartText = document.querySelector('.daggerheart');
 
 toggleBlisDigital.onclick = function() {
-    if (diceText.classList.contains('open')) {
-        diceText.classList.toggle('open');
+    if (bookText.classList.contains('open')) {
+        bookText.classList.toggle('open');
+    }
+    if (daggerheartText.classList.contains('open')) {
+        daggerheartText.classList.toggle('open');
     }
     blisDigitalText.classList.toggle('open');
 }
 
-toggleDice.onclick = function() {
+toggleBook.onclick = function() {
     if (blisDigitalText.classList.contains('open')) {
         blisDigitalText.classList.toggle('open');
     }
-    diceText.classList.toggle('open');
+    if (daggerheartText.classList.contains('open')) {
+        daggerheartText.classList.toggle('open');
+    }
+    bookText.classList.toggle('open');
+}
+
+toggleDaggerHeart.onclick = function() {
+    if (blisDigitalText.classList.contains('open')) {
+        blisDigitalText.classList.toggle('open');
+    }
+    if (bookText.classList.contains('open')) {
+        bookText.classList.toggle('open');
+    }
+    daggerheartText.classList.toggle('open');
 }
