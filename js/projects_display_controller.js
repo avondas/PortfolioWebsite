8f91d28f062ff6ac15e1142e6a3e9eaa128f0283
Avienda 't Hooft <aviendahooft@hotmail.com>
2025-10-29 14:34:54 +0100

    reworked projects displaying to remove duplicate code

---
+++ js/projects_display_controller.js
@@ -0,0 +1,17 @@
+﻿const allToggles = document.querySelectorAll('.project-toggle');
+const allContent = document.querySelectorAll('.project_content');
+
+allToggles.forEach(toggle => {
+    toggle.addEventListener('click', function() {
+        const targetId = this.getAttribute('data-target');
+        const targetContent = document.querySelector(targetId);
+
+        allContent.forEach(content => {
+            if (content !== targetContent && content.classList.contains('open')) {
+                content.classList.remove('open');
+            }
+        });
+
+        targetContent.classList.toggle('open');
+    });
+});
